de4d3abf30a8c7b4b98735d6a9a2789dc233c16e
Maksuma <max.bakurin@gmail.com>
2024-01-21 14:26:34 +0400

    Start ReactPizza project

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+	return (
+		<div className='flex flex-col '>
+			<h1 className='text-4xl text-center'>Hello World</h1>
+			<p className='text-center'>This is a simple React app.</p>
+		</div>
+	)
+}
